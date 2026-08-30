5d5506d86618425a5070f9fb44c90abbdcd487ff
MauricioPeralta-dot <peraltamauricio78@gmail.com>
2026-08-30 11:37:56 -0400

    feat: add exercise 02

---
+++ ejercicios/Ejercicio02.java
@@ -0,0 +1,22 @@
+public class Ejercicio02 {
+    public static void main(String[] args) {
+
+        // Declaración de variables
+        int a = 20;
+        int b = 5;
+
+        // Operaciones
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        int division = a / b;
+        int modulo = a % b;
+
+        // Mostrar resultados
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+    }
+}
